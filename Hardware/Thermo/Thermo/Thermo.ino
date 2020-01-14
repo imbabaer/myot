4342956d9509964d4b54d15b8f3b2b1cc7d17a85
Ruben <rubenmueller90@gmail.com>
2020-01-14 21:43:51 +0100

    new ssid and pw

--- Hardware/Thermo/Thermo/Thermo.ino
+++ Hardware/Thermo/Thermo/Thermo.ino
@@ -4,12 +4,10 @@
 
 //const char* ssid = "FRITZ!Box 7430 HV";
 //const char* password = "97364848481831321284";
-//const char* ssid = "ZyXEL WAP3205 v3";
-//const char* password = "USACG37344";
-//const char* ssid = "FRITZ!Box 7490i";
-//const char* password = "85215572252851731258";
-const char* ssid = "Ruben's iPhone";
-const char* password = "isz9mhpnc8cs1";
+const char* ssid = "Office Bonlanden";
+const char* password = "852355722528517399258";
+//const char* ssidalt = "FRITZ!Box 7490i";
+//const char* passwordalt = "85215572252851731258";
 
 const char* host = "192.168.178.21";
 const int httpPort = 80;
@@ -27,13 +25,13 @@ WiFiClientSecure client;
 Adafruit_BME280 bme;
 unsigned long delayTime;
 const String hostAdress = "http://xn--ruben-mller-zhb.de/uptime/temp.php";
-const String deviceID = "device1";
+const String deviceID = "device5";
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
-  while(!Serial);    // time to get serial running
-    Serial.println(F("BME280 test"));
+  while (!Serial);   // time to get serial running
+  Serial.println(F("BME280 test"));
   Serial.print("connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -46,81 +44,73 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-unsigned status;
-    
-    // default settings
-    // (you can also pass in a Wire library object like &Wire2)
-    status = bme.begin(0x76);  
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1);
-    }
-    Serial.println("-- Default Test --");
-    delayTime =  10000;//120000;//every two minues
+  unsigned status;
 
-    send();
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin(0x76);
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+    Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(), 16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1);
   }
+  Serial.println("-- Default Test --");
+  delayTime =  10000;//120000;//every two minues
+
+  send();
+}
 
 int value = 0;
-boolean firstrun = true;
-void loop() {}
-void send(){
- 
-if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
 
-printValues();
-    
- 
-HTTPClient http;  //Declare an object of class HTTPClient
- String str = hostAdress+"?device="+deviceID+"&value="+String(bme.readTemperature())+"&pressure="+String(bme.readPressure() / 100.0F)+"&humidity="+String(bme.readHumidity());
-http.begin(str);
-int httpCode = http.GET();
- 
-if (httpCode > 0) { //Check the returning code
- 
-String payload = http.getString();   //Get the request response payload
-Serial.println(payload);                     //Print the response payload
- 
+void loop() {}
+void send() {
+
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+
+    printValues();
+
+
+    HTTPClient http;  //Declare an object of class HTTPClient
+    String str = hostAdress + "?device=" + deviceID + "&value=" + String(bme.readTemperature()) + "&pressure=" + String(bme.readPressure() / 100.0F) + "&humidity=" + String(bme.readHumidity());
+    http.begin(str);
+    int httpCode = http.GET();
+
+    if (httpCode > 0) { //Check the returning code
+
+      String payload = http.getString();   //Get the request response payload
+      Serial.println(payload);                     //Print the response payload
+
+    }
+
+    http.end();   //Close connection
+
+  }
+  //delay(delayTime);
+  ESP.deepSleep(300e6);
 }
- 
-http.end();   //Close connection
- 
-}
- //delay(delayTime);
- //check if the two pins are not connected/ or check if this is the first run, for safely reflashing
- if(firstrun)
- {
-  firstrun = false;
-  delay(120000);
- }
- else{ 
- //deepsleep
- ESP.deepSleep(300e6); 
-}
-}
+
 
 void printValues() {
-    Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" *C");
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
 
-    Serial.print("Pressure = ");
+  Serial.print("Pressure = ");
 
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
 
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.println(" m");
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
 
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
 
-    Serial.println();
+  Serial.println();
 }
